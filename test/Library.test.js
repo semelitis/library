3bb4c3b98a9c791a246bcb1ff856ed6aedf2dcab
semelitis <80916733+semelitis@users.noreply.github.com>
2022-12-06 21:19:52 +0200

    on progress

--- test/Library.test.js
+++ test/Library.test.js
@@ -7,16 +7,9 @@ let isbn='123456789';
 let read='yes';
 
 let libraryTest=new Library();
-//the most function of this module dosent return anything
 
-test('add a book data to to the list', () => {
+it('add a book data to to the list', () => {
     let bookTest=new Book(title,author,isbn,read);
-    return expect(new Promise(libraryTest.addBookToList(bookTest)))
-    .resolves.toBe('made a book').catch(console.error('not made the expected book')
-    )});
+    expect(libraryTest.addBookToList(bookTest)).toBe('')
+});
 
-//here the for parameter the event object...
-test('remove a book form the list', () => {
-        return expect(new Promise(libraryTest.deleteBook()))
-        .resolves.toBe('book deleted succesfully').catch(console.error('the book hasnt beeb deleted ,still exists')
-        )});
